64781463efb3b0a89c763ad5d25d50f3b1d3c162
Electra Chong <electrachong@gmail.com>
2016-03-01 13:59:35 -0800

    Revise reply.js to use this keyword instead of var in for loop
    Using the variable was problematic because we would access its value after the for loop completed, meaning it would always be the last post instead of the post that was clicked.

--- reply.js
+++ reply.js
@@ -2,16 +2,17 @@ document.addEventListener("DOMContentLoaded", function() {
                                             
     var replies = document.querySelectorAll(".replyLink");
     var replyForms = document.querySelectorAll(".replyForm")
-        
+    
+  
     for (var i = 0; i < replies.length; i++) {
         console.log(i);
         var num = i;
-        replies[i].addEventListener("click", showReplyForm, num);
+        replies[i].addEventListener("click", showReplyForm);
     }
     
-    function showReplyForm(i) {
-        console.log(num);
-        var postnumber = replies[num].getAttribute("data-postlink");
+    function showReplyForm() {
+        console.log(this);
+        var postnumber = this.getAttribute("data-postlink");
         document.getElementById(postnumber).style.display = "initial";
     }
 })
