3e43fa510e55c3ce7861125b0cf06feb3cdf553b
madejean <madejean007@hotmail.fr>
2016-04-13 01:25:41 +0000

    php files

---
+++ login.php
@@ -0,0 +1,47 @@
+<?php session_start();
+      include_once('models/session_login.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+   <head>
+      <title>user login</title>
+       <link rel="stylesheet" type="text/css" href="twitter-clone.css">
+        <link rel="stylesheet" type="text/css" href="login.css">
+    </head>
+
+
+       <body>
+
+        <div id="container">
+
+            <header>
+                <div id="logoContainer">
+                    <img id="logo" src="http://twinsfunnfood.com/assets/twins/images/food.png" alt="logo" width="100" height="100">
+                    <p id="tagline">Share your nomz</p>
+                </div>
+
+                <h1>nomz.</h1>
+            </header>
+
+                 <h2>Login</h2>
+                  <div class = "container form-signin">
+		        </div>
+
+      <div class = "singincontainer">
+
+         <form class = "form-signin" role = "form"
+            action = "/index.php" method = "post">
+            <input type = "text" class = "form-control"
+               name = "username">
+            <input type = "password" class = "form-control"
+               name = "password">
+            <button class = "btn" type = "submit"
+               name = "login">Log in</button>
+          </form>
+        
+          </div>
+        </div>
+   </body>
+</html>
